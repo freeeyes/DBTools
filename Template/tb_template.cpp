f7edab4d7c0df9d990ab929573b815b1fcaffb52
freeeyes <freeeyes@163.com>
2019-06-06 16:33:28 +0800

    更新数据库读模板

--- Template/tb_template.cpp
+++ Template/tb_template.cpp
@@ -1,8 +1,8 @@
 #include "tb_[pbName].h"
 
-bool Save_aaa_Data(MYSQL* pConn, int nPalyerID, const char* sz[pbName], int nLen)
+bool Save_[pbName]_Data(MYSQL* pConn, int nPalyerID, const char* sz[pbName], int nLen)
 {
-	char szSQL[4096] = {'\0'}
+	char szSQL[8192] = {'\0'}
 
 	if(NULL == pConn)
 	{
@@ -17,9 +17,43 @@ bool Save_aaa_Data(MYSQL* pConn, int nPalyerID, const char* sz[pbName], int nLen
 	int ret = mysql_query(pConn, szSQL);
 	if(ret != 0)
 	{
-		printf("[Save_aaa_Data]error:%s\n", mysql_error(pConn));
+		printf("[Save_[pbName]_Data]error:%s\n", mysql_error(pConn));
 		return false;
 	}
 	return true;
 };
+
+bool Load_[pbName]_Data(MYSQL* pConn, int nPalyerID, char* sz[pbName], int nLen)
+{
+	char szSQL[500] = {'\0'}
+	MYSQL_ROW sql_row;
+
+	if(NULL == pConn)
+	{
+		return false;
+	}
+	
+	sprintf(szSQL, "select hex(userdata) tb_[pbName] where playerID='%s'", 
+			nPalyerID);	
+			
+	int ret = mysql_query(pConn, szSQL);
+	if(ret != 0)
+	{
+		printf("[Save_[pbName]_Data]error:%s\n", mysql_error(pConn));
+		return false;
+	}
+	
+	MYSQL_RES *result = mysql_store_result(pConn);
+	if(NULL != result)
+	{
+		while(sql_row = mysql_fetch_row(result))
+		{
+			strcpy(sz[pbName], sql_row[0]);
+			break;
+		}
+	}
+
+	mysql_free_result(result);
+	return true;
+}
 
--- Template/tb_template.h
+++ Template/tb_template.h
@@ -3,6 +3,8 @@
 
 #include "tb_Common.h"
 
-bool Save_[pbName]_Data(MYSQL* pConn, , int nPalyerID, const char* sz[pbName], int nLen);
+bool Save_[pbName]_Data(MYSQL* pConn, int nPalyerID, const char* sz[pbName], int nLen);
+
+bool Load_[pbName]_Data(MYSQL* pConn, int nPalyerID, char* sz[pbName], int nLen);
 
 #endif
